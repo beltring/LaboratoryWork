ca405bf51f611c904f2972ee22c381f47e32a455
Pavel Boltromyuk <beltring@tut.by>
2019-04-24 18:58:41 +0300

    Update logic method

--- Task5/src/by/bntu/fitr/poisit/task5/model/logic/Calculator.java
+++ Task5/src/by/bntu/fitr/poisit/task5/model/logic/Calculator.java
@@ -5,41 +5,39 @@ import by.bntu.fitr.poisit.task5.model.entity.SoundRecording;
 import java.util.Arrays;
 
 public class Calculator {
-    public static Music[] minPlayTime(SoundRecording recording){
-        if(recording == null || recording.size() < 1){
+
+    public static Music[] minPlayTime(SoundRecording recording) {
+        if (recording == null || recording.size() < 1) {
             return null;
         }
-        
+
         Music[] album = new Music[1];
         album[0] = recording.get(0);
-        
+
         for (int i = 0; i < recording.size(); i++) {
-            if(album[0].getTime() > recording.get(i).getTime()){
+            if (album[0].getTime() > recording.get(i).getTime()) {
                 album = new Music[0];
-                
-                album = Arrays.copyOf(album, album.length + 1);
-                album[album.length -1] = recording.get(i);
             }
-            else if(album[0].getTime() == recording.get(i).getTime()){
+            if (album[0].getTime() >= recording.get(i).getTime()) {
                 album = Arrays.copyOf(album, album.length + 1);
-                album[album.length -1] = recording.get(i);
-            }  
+                album[album.length - 1] = recording.get(i);
+            }
         }
-        
+
         return album;
     }
-    
-    public static int totalTime(SoundRecording recording){
+
+    public static int totalTime(SoundRecording recording) {
         int total = 0;
-        
-        if(recording == null){
+
+        if (recording == null) {
             return total;
         }
-        
+
         for (int i = 0; i < recording.size(); i++) {
             total += recording.get(i).getTime();
         }
-        
+
         return total;
     }
 }
--- Task5/test/by/bntu/fitr/poisit/task5/model/logic/CalculatorTest.java
+++ Task5/test/by/bntu/fitr/poisit/task5/model/logic/CalculatorTest.java
@@ -5,63 +5,61 @@ import by.bntu.fitr.poisit.task5.model.entity.Music;
 import by.bntu.fitr.poisit.task5.model.entity.Popular;
 import by.bntu.fitr.poisit.task5.model.entity.Rock;
 import by.bntu.fitr.poisit.task5.model.entity.SoundRecording;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CalculatorTest {
-    
+
     public CalculatorTest() {
     }
-    
-    Music m1 = new Rock("alternative rock",115,99,"Rammstein");
-    Music m2 = new Popular(10_500_000, 1, 126,"KAZKA");
+
+    Music m1 = new Rock("alternative rock", 115, 99, "Rammstein");
+    Music m2 = new Popular(10_500_000, 1, 126, "KAZKA");
     Music m3 = new Classic(4, 90, "Paul Mauriat");
-    Music m4 = new Popular(9_320_000, 3, 100,"Max Korzh");
+    Music m4 = new Popular(9_320_000, 3, 100, "Max Korzh");
     Music m5 = new Classic(1, 90, "Ludwig van Beethoven");
-    
+
     Music[] album = {m1, m2, m3, m4, m5};
-    
+
     SoundRecording recording = null;
     SoundRecording recording1 = new SoundRecording("EverestMedia", album);
     SoundRecording recording2 = new SoundRecording("EverestMedia");
-    
+
     @Test
     public void testMinPlayTime() {
         Music[] expResult = null;
         Music[] result = Calculator.minPlayTime(recording);
         assertArrayEquals(expResult, result);
     }
-    
+
     @Test
     public void testMinPlayTime1() {
         Music[] expResult = {m3, m5};
         Music[] result = Calculator.minPlayTime(recording1);
         assertArrayEquals(expResult, result);
     }
-    
+
     @Test
     public void testMinPlayTime2() {
         Music[] expResult = null;
         Music[] result = Calculator.minPlayTime(recording2);
         assertArrayEquals(expResult, result);
     }
-    
+
     @Test
     public void testTotalTime() {
         int expResult = 0;
         int result = Calculator.totalTime(recording);
         assertEquals(expResult, result);
     }
-    
+
     @Test
     public void testTotalTime1() {
         int expResult = 505;
         int result = Calculator.totalTime(recording1);
         assertEquals(expResult, result);
     }
-    
+
     @Test
     public void testTotalTime2() {
         int expResult = 0;
